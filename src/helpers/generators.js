93c27eb98b8de16208c72e64d60e5dd22d2bd4ba
anni shubitidze <anishubitidze1998@gmail.com>
2023-02-09 00:07:32 +0400

    experience commit

--- src/helpers/generators.js
+++ src/helpers/generators.js
@@ -1,8 +1,11 @@
-export function generateErrors(err, container) {
-    err.map((item) => {
-      const newPTag = document.createElement("p");
-      newPTag.innerHTML = `${item}`;
-      newPTag.style.color = "red";
-      container.appendChild(newPTag);
-    });
-  }
+import { removeLocalStorage } from "../helpers/populateLocalStorage.js";
+
+export function generateErrors(type, err, container) {
+  removeLocalStorage(type);
+  err.map((item) => {
+    const newPTag = document.createElement("p");
+    newPTag.innerHTML = `${item}`;
+    newPTag.style.color = "red";
+    container.appendChild(newPTag);
+  });
+}
